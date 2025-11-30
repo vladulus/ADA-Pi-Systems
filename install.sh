9c82bab92e229c4ab118e27e18c5faaa6c8ff420
vladulus <vladulus@users.noreply.github.com>
2025-11-30 13:19:49 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -71,7 +71,7 @@ if [[ "$NETOPT" == "1" || "$NETOPT" == "3" ]]; then
     sudo mkdir -p --mode=0755 /usr/share/keyrings
     curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
 
-    echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflared.list
+    echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared bookworm main" | sudo tee /etc/apt/sources.list.d/cloudflared.list
 
     sudo apt update
     sudo apt install -y cloudflared
